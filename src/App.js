7d0fe052de2923cbd3bc4d7de1a794fab956f8b6
marksfrontdev <marcosprojetosdev@gmail.com>
2019-10-17 06:24:36 -0300

    Feat: Created Routes, Pages for Main and Repository

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
+import Routes from "./routes"
 
 function App() {
   return (
-    <div>
-      <h1>Project</h1>
-    </div>
+    <Routes />
   );
 }
 
